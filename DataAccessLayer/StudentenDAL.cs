3d43683a0ef1b66cb4eadc02ed6c939e36e42c70
Nicholas Greenslade <ng.greenslade@gmail.com>
2017-06-21 09:46:00 +0200

    Add files via upload

---
+++ DataAccessLayer/StudentenDAL.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Classes.cs;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class StudentenDAL
+    {
+        public static List<Studenten> StudentenLijst()
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT studentID, voorNaam, achterNaam FROM Studenten", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Studenten> klas = new List<Studenten>();
+           
+
+            try
+            {
+                while(reader.Read())
+                {
+                    Studenten student = new Studenten();
+                    if (!reader.IsDBNull(0)) student.studentID = reader.GetInt32(0);
+                    if (!reader.IsDBNull(1)) student.voorNaam = reader.GetString(1);
+                    if (!reader.IsDBNull(2)) student.achterNaam = reader.GetString(2);
+                    klas.Add(student);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return klas;
+        }
+
+        public static string GetLastName(Studenten student)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT achterNaam FROM Studenten WHERE studentID = @studentID", con);
+            cmd.Parameters.AddWithValue("@studentID", student.studentID);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while(reader.Read())
+                {
+                    if (!reader.IsDBNull(0)) student.achterNaam = reader.GetString(0);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }        
+            return student.achterNaam;
+        }
+
+        public static string GetFirstName(Studenten student)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT voorNaam FROM Studenten WHERE studentID = @studentID", con);
+            cmd.Parameters.AddWithValue("@studentID", student.studentID);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0)) student.voorNaam = reader.GetString(0);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+           
+            return student.voorNaam;
+        }
+
+        public static void AddStudentToDatabase(Studenten student)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("INSERT INTO Studenten (voorNaam, achterNaam) VALUES (@voorNaam, @achterNaam)", con);
+            cmd.Parameters.AddWithValue("@voorNaam", student.voorNaam);
+            cmd.Parameters.AddWithValue("@achterNaam", student.achterNaam);
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public static List<Vakken> GetStudent(Studenten student)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Vakken JOIN StudentVak ON StudentVak.vakID = Vakken.vakID WHERE StudentVak.studentID = @studentID", con);
+            cmd.Parameters.AddWithValue("@studentID", student.studentID);
+            List<Vakken> vakken = new List<Vakken>();
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                while(reader.Read())
+                {
+                    Vakken vak = new Vakken();
+                    if (!reader.IsDBNull(0)) vak.vakID = reader.GetInt32(0);
+                    if (!reader.IsDBNull(1)) vak.vakNaam = reader.GetString(1);
+                    vakken.Add(vak);
+                    
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return vakken;
+            
+        }
+    }
+}
---
+++ DataAccessLayer/VakkenDAL.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Classes.cs;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DataAccessLayer
+{
+    public class VakkenDAL
+    {
+        public static List<Vakken> VakkenLijst()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Vakken", con);
+            List<Vakken> vakkenlijst = new List<Vakken>();
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    Vakken vak = new Vakken();
+                    if (!reader.IsDBNull(0)) vak.vakID = reader.GetInt32(0);
+                    if (!reader.IsDBNull(1)) vak.vakNaam = reader.GetString(1);
+                    vakkenlijst.Add(vak);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return vakkenlijst;
+
+        }
+
+        public static void AddVakToDatabase(string vaknaam)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("INSERT INTO Vakken (vakNaam) VALUES (@vakNaam)", con);
+            cmd.Parameters.AddWithValue("@vakNaam", vaknaam);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static void UpdateVak(Vakken vak)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("UPDATE Vakken SET vakNaam = @vakNaam WHERE vakID = @vakID", con);
+            cmd.Parameters.AddWithValue("@vakNaam", vak.vakNaam);
+            cmd.Parameters.AddWithValue("@vakID", vak.vakID);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        public static List<Studenten> ShowStudentenVanVak(Vakken vak)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("SELECT Studenten.studentID, voorNaam, achternaam FROM Studenten JOIN StudentVak ON StudentVak.studentID = Studenten.studentID WHERE StudentVak.vakID = @vakID", con);
+            cmd.Parameters.AddWithValue("@vakID", vak.vakID);
+            List<Studenten> students = new List<Studenten>();
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            try
+            {
+                while (reader.Read())
+                {
+                    Studenten student = new Studenten();
+                    if (!reader.IsDBNull(0)) student.studentID = reader.GetInt32(0);
+                    if (!reader.IsDBNull(1)) student.voorNaam = reader.GetString(1);
+                    if (!reader.IsDBNull(2)) student.achterNaam = reader.GetString(2);
+                    students.Add(student);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return students;
+
+        }
+
+        public static void VoegVakToeAanStudent(Vakken vak, Studenten student)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-698P2MHO\SQLEXPRESS;Initial Catalog=StudentenAdministratie;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("INSERT INTO StudentVak VALUES (@studentID, @vakID)", con);
+            cmd.Parameters.AddWithValue("@studentID", student.studentID);
+            cmd.Parameters.AddWithValue("@vakID", vak.vakID);
+            try
+            {
+                con.Open();
+                cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
